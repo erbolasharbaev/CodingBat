5bb250cddb660e0431fdf40fda9d36d0e13c4772
Erbol <erbola14@gmail.com>
2026-07-16 20:45:23 +0600

    Mix Start

---
+++ src/main/java/org/spring/warmup_1/MixStart.java
@@ -0,0 +1,10 @@
+package org.spring.warmup_1;
+
+public class MixStart {
+    public boolean mixStart(String str) {
+        if (str.startsWith("ix", 1)) {
+            return true;
+        }
+        return false;
+    }
+}
