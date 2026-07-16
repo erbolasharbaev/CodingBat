b3059ffe12291dccff1c559aeb9e67b35f7db272
Erbol <erbola14@gmail.com>
2026-07-16 15:21:18 +0600

    StartHi

--- src/main/java/org/spring/warmup_1/StartHi.java
+++ src/main/java/org/spring/warmup_1/StartHi.java
@@ -2,6 +2,7 @@ package org.spring.warmup_1;
 
 public class StartHi {
     public boolean startHi(String str) {
+        int sum = 0;
         boolean re = false;
         if (str.startsWith("hi")) {
             re = true;
