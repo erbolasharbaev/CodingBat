1ef77144d8209052c3d793e8641cf0e1febf5f49
Erbol <erbola14@gmail.com>
2026-07-16 14:16:59 +0600

    Sum Double

--- src/main/java/org/spring/warmup_1/SumDouble.java
+++ src/main/java/org/spring/warmup_1/SumDouble.java
@@ -1,4 +1,14 @@
 package org.spring.warmup_1;
 
 public class SumDouble {
+    public int sumDouble(int a, int b) {
+
+        int sum = 0;
+        if (a == b) {
+            sum = (a + b) * 2;
+        } else {
+            sum = a + b;
+        }
+        return sum;
+    }
 }
