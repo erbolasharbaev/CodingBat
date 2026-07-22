6758cdeacbbcaba7b87610553b08fa41f511f5c2
Erbol <erbola14@gmail.com>
2026-07-22 09:18:02 +0600

    EndUp

--- src/main/java/org/spring/warmup_1/EndUp.java
+++ src/main/java/org/spring/warmup_1/EndUp.java
@@ -2,6 +2,7 @@ package org.spring.warmup_1;
 
 public class EndUp {
     public String endUp(String str) {
+        int i = 0;
         StringBuilder sb = new StringBuilder();
         if(str.length() >= 3){
             sb.append(str.substring(0,str.length() - 3)).
