1e8dfbc424984a891921975925cef49be093cfb5
Erbol <erbola14@gmail.com>
2026-07-22 10:00:44 +0600

    Bye bye Argentina

--- src/main/java/org/spring/Argentina.java
+++ src/main/java/org/spring/Argentina.java
@@ -2,6 +2,6 @@ package org.spring;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("Argentina");
+        System.out.println(" bye bye Argentina");
     }
 }
